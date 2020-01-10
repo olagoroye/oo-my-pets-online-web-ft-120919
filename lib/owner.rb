c91d21da26ffc67c15a8d204baa8d657a3cfb762
olusola olagoroye <solaolagoroye@gmail.com>
2020-01-10 23:31:44 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -51,13 +51,13 @@ class Owner
   end
   
 def sell_pets
-  Dog.all.each do |dog| 
+  dogs.each do |dog| 
     dog.mood = "nervous" 
     dog.owner = nil
   
  end
     
-  Cat.all.each do |cat| 
+  cats.each do |cat| 
     cat.mood = "nervous" 
     cat.owner = nil
   end
