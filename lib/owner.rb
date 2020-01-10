989a481cc9759dcfdb0d489b51f1a6ecb145ced3
olusola olagoroye <solaolagoroye@gmail.com>
2020-01-10 19:29:42 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,35 @@
 class Owner
-  # code goes here
+  attr_accessor :cats, :dogs
+  attr_reader :name, :species
+  @@all = []
+ 
+  
+  def initialize(name)
+  @name = name
+  @species = "human"
+  @@all << self
+  @cats = []
+  @dogs = []
+  end
+
+  def say_species
+  "I am a #{@species}."
+  end
+  def self.all
+  @@all
+  end 
+  def self.count
+  self.all.count
+  end
+  def self.reset_all
+  @@all.clear
+  end
+  def buy_cat(name)
+    Cat.name.count {|cat| cat.name == self}
+  
+  end
 end
+
+# @owner.cats.each do |cat|
+#           expect(cat).to be_a(Cat)
+
