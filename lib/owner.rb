ab0f8ae000ecfdf53fa3bcdda0cf3f48dc1f4733
olusola olagoroye <solaolagoroye@gmail.com>
2020-01-10 23:32:21 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,5 +1,5 @@
 class Owner
-  attr_accessor :cats, :dogs
+  
   attr_reader :name, :species
   @@all = []
  
@@ -8,26 +8,64 @@ class Owner
   @name = name
   @species = "human"
   @@all << self
-  @cats = []
-  @dogs = []
+  
   end
-
+  
+  def cats 
+  Cat.all.select { |cat| cat.owner == self}
+    
+  end
+  
+  def dogs
+    Dog.all.select { |dog| dog.owner == self}
+  end
+  
   def say_species
   "I am a #{@species}."
   end
+  
   def self.all
   @@all
-  end 
+  end
+  
   def self.count
   self.all.count
   end
+  
   def self.reset_all
   @@all.clear
   end
+  
   def buy_cat(name)
-    Cat.name.count {|cat| cat.name == self}
+    Cat.new(name, self)
   
   end
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
+  def walk_dogs
+    Dog.all.each { |dog| dog.mood = "happy" if dog.owner == self }
+  end
+  def feed_cats
+    Cat.all.each { |cat| cat.mood = "happy" if cat.owner == self }
+  end
+  
+def sell_pets
+  dogs.each do |dog| 
+    dog.mood = "nervous" 
+    dog.owner = nil
+  
+ end
+    
+  cats.each do |cat| 
+    cat.mood = "nervous" 
+    cat.owner = nil
+  end
+
+end
+def list_pets
+  "I have #{dogs.count} dog(s), and #{cats.count} cat(s)."
+end
 end
 
 # @owner.cats.each do |cat|
